35b1d004d912e78ca7ff6568f8230007064a555b
Andrewkk0139 <andrewkaiser2006@gmail.com>
2023-02-28 19:58:16 -0600

    2nd grey box not colliding

---
+++ DrWormFort-AK/Assets/Scripts/GameOver.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public static bool playerTouchedLaser;
+    public GameObject player;
+    private Animator animator;
+    public AudioClip teleportHum;
+    public AudioClip laserHit;
+
+    private AudioSource[] allAudioSources;
+    private AudioSource teleporterAudio;
+
+    private void Awake()
+    {
+        teleporterAudio = GetComponent<AudioSource>();
+        teleporterAudio.clip = teleportHum;
+        teleporterAudio.volume = 0.4f;
+        teleporterAudio.Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = player.GetComponent<Animator>();
+        playerTouchedLaser = !Laser.playerDeath;
+        allAudioSources = FindObjectsOfType<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!teleporterAudio.isPlaying)
+        {
+            teleporterAudio.Play();
+        }
+    }
+
+    public void PlayerHit()
+    {
+        //Audio for hitting the player
+        teleporterAudio.PlayOneShot(laserHit, 1.0f);
+        StopAllAudio();
+
+        if (playerTouchedLaser)
+        {
+        //when the bool playerTouchedLaser is triggered,
+        // were calling the animator and having it play the playerHit animation
+        // Then we give the player time to do the animation, then it we call
+        // the restartLevel method
+            animator.SetTrigger("PlayerHit");
+            Invoke("RestartLevel",5);
+        }
+    }
+
+    public void StopAllAudio()
+    {
+        for(var audioS = 0; audioS < allAudioSources.Length; audioS++)
+        {
+            //stop the footsteps from dead player
+            if (allAudioSources[audioS] != teleporterAudio)
+            {
+                allAudioSources[audioS].Stop();
+            }
+        }      
+
+    }
+    public void RestartLevel()
+    {
+        Debug.Log("Oh no!");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
